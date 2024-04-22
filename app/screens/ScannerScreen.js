39ea178218ae0f51c9f347f928a1eca540437d28
George Charitonidis <gc@icloud.gr>
2024-04-22 11:10:49 +0300

    Playing with ScannerScreen 1

--- app/screens/ScannerScreen.js
+++ app/screens/ScannerScreen.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Screen from './Screen';
+import colors from '../config/colors';
 
-function ScannerScreen({}) {
+function ScannerScreen() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [scannedData, setScannedData] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync(); // Updated method
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        setScannedData(data);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}></Text>
-        </View>
+        <Screen>
+            <View style={styles.container}>
+                <Camera
+                    style={styles.camera}
+                    type={Camera.Constants.Type.back}
+                    onBarCodeScanned={
+                        scanned ? undefined : handleBarCodeScanned
+                    }
+                />
+                {scanned && (
+                    <Text style={styles.text}>
+                        Scanned QR Code: {scannedData}
+                    </Text>
+                )}
+                {scanned && (
+                    <Button
+                        title={'Tap to Scan Again'}
+                        onPress={() => setScanned(false)}
+                    />
+                )}
+            </View>
+        </Screen>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    text: {},
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    camera: {
+        height: '100%',
+        flex: 1,
+        width: '100%',
+    },
+    text: {
+        color: colors.white,
+        fontFamily: 'monospace',
+    },
 });
 
 export default ScannerScreen;
