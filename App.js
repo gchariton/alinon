50b063351124807185d754cad708592fc57f0ee8
George Charitonidis <gc@icloud.gr>
2024-05-22 13:01:03 +0300

    Trying to create a standalone app

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomBarNav from './app/components/BottomBarNav';
 import { AuthProvider } from './app/auth/authContext';
 
+import { enableScreens } from 'react-native-screens';
+enableScreens();
+
 export default function App() {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
